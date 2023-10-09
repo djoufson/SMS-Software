4d475f6519cdc445f29594aa6084ca11de56026f
Djoufson CHE BENE <djouflegran@gmail.com>
2023-10-09 16:31:21 +0100

    setup: Setting up the backend api project

---
+++ src/api/DependencyInjection.cs
@@ -0,0 +1,36 @@
+namespace Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddCors();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        return services;
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors(cfg =>
+        {
+            cfg.AllowAnyOrigin();
+        });
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,25 +1,10 @@
+using Api;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.RegisterServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.ConfigurePipeline();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
